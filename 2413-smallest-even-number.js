0fc58aeb8e694e2871919b532ab7413906baf743
Patrick Speranza <patrick.m.speranza@gmail.com>
2023-02-12 12:45:52 -0600

    1431 kids with candies

--- 2413-smallest-even-number.js
+++ 2413-smallest-even-number.js
@@ -18,3 +18,16 @@
 const smallestEvenMultiple = function (n) {
   return n % 2 === 0 ? n : n * 2
 }
+
+const smallestEvenMultipleOption2 = function (n) {
+  let multipleFound = false
+  let i = 1
+  while (multipleFound === false) {
+    if ((n * i) % 2 === 0) {
+      multipleFound = true
+    } else {
+      i++
+    }
+  }
+  return i * n
+}
