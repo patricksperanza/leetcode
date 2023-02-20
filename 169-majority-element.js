decce41d24fb03abf019994287b4491e36064db7
Patrick Speranza <patrick.m.speranza@gmail.com>
2023-02-20 14:17:55 -0600

    169 majority element

---
+++ 169-majority-element.js
@@ -0,0 +1,27 @@
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+const majorityElement = function (nums) {
+  let count = 0
+  let candidate
+
+  for (let num of nums) {
+    if (count === 0) {
+      candidate = num
+    }
+
+    count += num === candidate ? 1 : -1
+  }
+
+  return candidate
+}
