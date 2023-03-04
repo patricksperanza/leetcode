3739ebb5b3dbececd88fcae53647ec703decd64e
Patrick Speranza <patrick.m.speranza@gmail.com>
2023-03-04 13:10:44 -0600

    1403 minimum subsequence

---
+++ 1403-minimum-subsequence.js
@@ -0,0 +1,24 @@
+// Given the array nums, obtain a subsequence of the array whose sum of elements is strictly greater than the sum of the non included elements in such subsequence.
+
+// If there are multiple solutions, return the subsequence with minimum size and if there still exist multiple solutions, return the subsequence with the maximum total sum of all its elements. A subsequence of an array can be obtained by erasing some (possibly zero) elements from the array.
+
+// Note that the solution with the given constraints is guaranteed to be unique. Also return the answer sorted in non-increasing order.
+
+// Example 1:
+
+// Input: nums = [4,3,10,9,8]
+// Output: [10,9]
+// Explanation: The subsequences [10,9] and [10,8] are minimal such that the sum of their elements is strictly greater than the sum of elements not included. However, the subsequence [10,9] has the maximum total sum of its elements.
+
+const minSubsequence = function (nums) {
+  nums.sort((a, b) => b - a)
+  let first = 0
+  let second = 1
+  let i = 1
+  while (first <= second) {
+    first = nums.slice(0, i).reduce((acc, c) => acc + c, 0)
+    second = nums.slice(i).reduce((acc, c) => acc + c, 0)
+    i++
+  }
+  return nums.slice(0, i - 1)
+}
